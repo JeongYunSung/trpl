db8759f77b917f0aebd30d8d43720ff39a2c37f2
JeongYunSung <123dbstjd@gmail.com>
2023-06-12 22:58:27 +0900

    refactor: multi pacakage chaged to workspace

--- src/workspace/add_two/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn add_two(x: i32, y: i32) -> i32 {
-    x + y
-}
--- src/workspace/adder/src/main.rs
+++
@@ -1,9 +0,0 @@
-use add_one;
-use add_two;
-
-fn main() {
-    let x = 10;
-    let y = 20;
-    println!("add one : {}", add_one::add_one(x));
-    println!("add two : {}", add_two::add_two(x, y));
-}
