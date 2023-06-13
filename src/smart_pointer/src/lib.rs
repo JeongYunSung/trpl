5a9a21f7bb5d21685dd876772a98aab882d5bc75
JeongYunSung <123dbstjd@gmail.com>
2023-06-13 20:24:23 +0900

    feature: refcell and rc

--- src/smart_pointer/src/lib.rs
+++ src/smart_pointer/src/lib.rs
@@ -1,4 +1,6 @@
 use std::ops::Deref;
+use std::rc::Rc;
+use crate::List::{Cons, Nil};
 
 pub struct MyBox<T>(T);
 
@@ -14,4 +16,21 @@ impl<T> Deref for MyBox<T> {
     fn deref(&self) -> &Self::Target {
         &self.0
     }
+}
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+pub fn print_rc_pointer() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
--- src/smart_pointer/src/main.rs
+++ src/smart_pointer/src/main.rs
@@ -1,21 +1,5 @@
-use smart_pointer::MyBox;
-
-struct CustomSmartPointer {
-    data: String,
-}
-
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+use smart_pointer::*;
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
+    print_rc_pointer();
 }
