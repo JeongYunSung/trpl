d06e546e1ab9ef295fd177af367e850ff4517bd2
JeongYunSung <123dbstjd@gmail.com>
2023-06-16 09:32:42 +0900

    feature: optimized grep

--- src/grep/src/main.rs
+++ src/grep/src/main.rs
@@ -1,13 +1,11 @@
 extern crate grep;
 
-use std::{env};
+use std::env;
+
 use grep::*;
 
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1)
     });
