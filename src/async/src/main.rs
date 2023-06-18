3e2459c6595d81b1c8e08b039c2587df3dce6d77
JeongYunSung <123dbstjd@gmail.com>
2023-06-18 12:15:07 +0900

    feature: module name chagend tokio to async, create new module async_client

--- src/async/src/main.rs
+++ src/async/src/main.rs
@@ -3,22 +3,31 @@
 //! 토키오는 하나의 작업에 대해 병렬처리하거나 Thread Pool을 이용해야 할 때를 제외하곤 사용할 수 있다
 //! 토키오는 async-std와 달리 FFI 지원이 없다.
 
-extern crate mini_redis;
 extern crate tokio;
+extern crate mini_redis;
 
-use mini_redis::{client, Result};
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
 
 /// 런타임시점에 main을 호출하고 프로그램을 진행해야 하는데, main함수가 async면 호출되자마자 종료되기에 사용할 수 없음
 /// #[tokio::main]은 tokio에서 제공하는 매크로로 async func에 붙일 시 자동으로 blocking을 처리해줌
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?; // connecting to the server in localhost
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    client.set("hello", "world".into()).await?;
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
 
-    let result = client.get("hello").await?;
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
 
-    println!("got value from the server; result={:?}", result);
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
 
-    Ok(())
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
---
+++ src/async_client/src/main.rs
@@ -0,0 +1,16 @@
+extern crate mini_redis;
+
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?; // connecting to the server in localhost
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
