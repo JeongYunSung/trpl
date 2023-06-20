4cf14d3cc565be0c72fe893c5550c54e21066d85
JeongYunSung <123dbstjd@gmail.com>
2023-06-20 23:46:34 +0900

    feature: create asynchronized tokio loop by using channel, waker

--- src/tokio_study/src/my_executors.rs
+++ src/tokio_study/src/my_executors.rs
@@ -7,9 +7,12 @@
 use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 use std::task::Context;
 use std::time::{Duration, Instant};
+use crossbeam::channel;
 use futures::task;
+use futures::task::ArcWake;
 use super::Delay;
 
 pub fn execute() {
@@ -23,6 +26,7 @@ pub fn execute() {
 
         let out = future.await;
         assert_eq!(out, "done");
+        println!("Result : {}", out);
     });
 
     println!("middle");
@@ -32,25 +36,86 @@ pub fn execute() {
 }
 
 struct MiniTokio {
-    tasks: VecDeque<Task>,
+    scheduled: channel::Receiver<Arc<Task>>,
+    sender: channel::Sender<Arc<Task>>,
 }
 
-type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+struct Task {
+    // Task에 대해 Sync를 구현하기 위해 Mutex를 사용
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>,
+}
+
+impl Task {
+    fn schedule(self: &Arc<Self>) {
+        let _ = self.executor.send(self.clone());
+    }
+
+    fn poll(self: Arc<Self>) {
+        let waker = task::waker(self.clone());
+        let mut cx = Context::from_waker(&waker);
+
+        let mut future = self.future.try_lock().unwrap();
+
+        let _ = future.as_mut().poll(&mut cx);
+    }
+
+    fn spawn<F>(future: F, sender: &channel::Sender<Arc<Task>>)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+            executor: sender.clone(),
+        });
+
+        let _ = sender.send(task);
+    }
+}
+
+// Waker를 hooking하기 위해선 low level API인 vtable을 사용해나,
+// 이 부분은 손이 많이 가기에 futures의 ArcWake Trait을 사용한다.
+impl ArcWake for Task {
+
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        arc_self.schedule();
+    }
+}
 
 impl MiniTokio {
     fn new() -> MiniTokio {
-        MiniTokio {
-            tasks: VecDeque::new(),
-        }
+        let (sender, scheduled) = channel::unbounded();
+
+        MiniTokio { scheduled, sender }
     }
 
     fn spawn<F>(&mut self, future: F)
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        self.tasks.push_back(Box::pin(future));
+        Task::spawn(future, &self.sender);
     }
 
+    /// Waker방식을 적용할 때 Channel을 이용할 것 이다.
+    /// 하지만 std channel의 경우 sync가 구현이 되어있지 않아 사용할 수 없다.
+    /// 따라서 crossbeam의 channel을 이용해야 한다.
+    fn run(&self) {
+        while let Ok(task) = self.scheduled.recv() {
+            task.poll();
+        }
+    }
+}
+
+type OldTask = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+struct OldMiniTokio {
+    tasks: VecDeque<OldTask>,
+}
+
+impl OldMiniTokio {
+    /// 상당히 비효율적인 방식의 executor이다.
+    /// 왜냐하면 이는 단순 무식하게 instance를 꺼내와서 polling하고 상태 확인하고 ready면 끝내고 아니면 다시 queue에 넣는 방식으로
+    /// 이는 cpu사용률을 증진시키기에 최적화된 구조이다.
     fn run(&mut self) {
         let waker = task::noop_waker();
         let mut cx = Context::from_waker(&waker);
--- src/tokio_study/src/my_future.rs
+++ src/tokio_study/src/my_future.rs
@@ -7,6 +7,7 @@
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use std::thread;
 use std::time::Instant;
 
 pub struct Delay {
@@ -16,13 +17,43 @@ pub struct Delay {
 impl Future for Delay {
     type Output = &'static str;
 
+    /// Waker란 해당 Task가 작업을 다시 실행할 수 있다고 외부에 알릴 수 있게 도와주는 구조체이다.
+    /// Waker는 Task내에 Context에 등록되어 있어 Context를 활용해 가져올 수 있다.
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         if Instant::now() >= self.when {
             println!("Hello world");
             Poll::Ready("done")
         } else {
-            cx.waker().wake_by_ref();
+            let waker = cx.waker().clone();
+            let when = self.when;
+
+            thread::spawn(move || {
+                let now = Instant::now();
+
+                if now < when {
+                    thread::sleep(when - now);
+
+                    waker.wake();
+                }
+            });
+
             Poll::Pending
+        }
+    }
+}
+
+impl Delay {
+
+    /// Waker를 사용하지 않은 개선되기 이전에 poll
+    fn not_optimized_poll(self: Pin<&mut Self>, cx: &mut Context<'_>) {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            let _ = Poll::Ready("done");
+        } else {
+            // 아래 wake_by_ref는 효과적이고 암묵적이게 소유한 Waker를 이용해서 wake처리를 한다.
+            // 다만 이는 필요이상의 CPU사이클이 낭비될 수 있다.
+            cx.waker().wake_by_ref();
+            // Poll::Pending;
         }
     }
 }
