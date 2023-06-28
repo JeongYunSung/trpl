ccdec722f67230caf902db42265de326252dd19e
JeongYunSung <123dbstjd@gmail.com>
2023-06-28 17:48:15 +0900

    feature: Implementing Universe

--- src/game_of_life/www/index.js
+++ src/game_of_life/www/index.js
@@ -1,3 +1,13 @@
-import * as wasm from "game_of_life";
+import { Universe } from "game_of_life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
